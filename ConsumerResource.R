44b9f417c89b15c8911224dcab6e5e29c35d4b1f
hengxingzou <19738417+hengxingzou@users.noreply.github.com>
2023-06-12 21:59:24 -0500

    Jun 2023 Upload

---
+++ ConsumerResource.R
@@ -0,0 +1,351 @@
+# Simulating Priority Effects in a Consumer-Resource Model
+# Hengxing Zou
+
+# packages
+library(tidyverse)
+library(magrittr)
+library(patchwork)
+library(doParallel)
+registerDoParallel(cores = 4)
+
+# color scheme from https://jfly.uni-koeln.de/color/
+color_scheme = c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+##### Functions #####
+
+# The Model
+
+model_kl = function(time, state, pars, deltap, type) {
+  
+  all_pop = tibble()
+  
+  # type = 1: trait-mediated priority effects (k_i changes with \Delta p)
+  # type = 0: numeric priority effects (k_i constant)
+  
+  k1 = type*get_k(2*k1, scal, xmid, deltap) + (1-type)*k1
+  k2 = type*get_k(2*k2, -scal, xmid, deltap) + (1-type)*k2
+  
+  for (t in 1:time) {
+    
+    new_state = with(as.list(c(state, pars)), {
+      
+      new_R = r*R*(1-R/K)+R-Q1*lambda1*N1*R^2/(k1+R^2)-Q2*lambda2*N2*R/(k2+R+R^2/ki2)
+      new_N1 = lambda1*N1*R^2/(k1+R^2)+(1-mu1)*N1
+      new_N2 = lambda2*N2*R/(k2+R+R^2/ki2)+(1-mu2)*N2
+      
+      c(N1 = new_N1, N2 = new_N2, R = new_R)
+    })
+    
+    all_pop = rbind(all_pop, tibble(Time = t, 
+                                    N1 = new_state["N1"], N2 = new_state["N2"], R = new_state["R"]))
+    state = new_state
+    
+  }
+  
+  return(all_pop)
+}
+
+# Trait-mediated Priority Effects
+
+get_k = function(base_k, scal, xmid, deltap) {
+  return(base_k/(1+exp((xmid-deltap)/scal)))
+}
+
+# Simulating Early/Late Arrival over Generations
+
+arrival_time = function(time_total, time_early, init_pop, deltap, type) {
+  
+  time_both = time_total-abs(time_early)
+  
+  if (time_early == 0) {
+    out = model_kl(time_both, init_pop, pars = NULL, deltap, type)
+    return(out)
+  } 
+  
+  else {
+    
+    late_spp = if_else(time_early < 0, 2, 1)
+    late_spp_init = init_pop[late_spp]
+    init_pop[late_spp] = 0
+    
+    out1 = model_kl(abs(time_early), init_pop, pars = NULL, deltap, type)
+    
+    init_pop["N1"] = as.numeric(tail(out1, 1)[, 2])
+    init_pop["N2"] = as.numeric(tail(out1, 1)[, 3])
+    init_pop["R"] = as.numeric(tail(out1, 1)[, 4])
+    init_pop[late_spp] = late_spp_init
+    
+    out2 = model_kl(time_both, init_pop, pars = NULL, deltap, type)
+    
+    return(rbind(out1, mutate(out2, Time = Time+abs(time_early))))
+    
+  }
+}
+
+##### Parameters #####
+
+lambda1 = 0.029
+lambda2 = 0.2
+k1 = 0.02
+k2 = 3
+Q1 = 0.01
+Q2 = 0.01
+mu1 = mu2 = 0.01
+ki2 = 1
+
+r = 0.5
+K = 3
+
+scal = 7.5
+xmid = 0
+
+init_pop = c(N1 = 5, N2 = 5, R = K)
+
+##### Simulation I. Numeric and Trait-mediated Priority Effects #####
+
+# Species 2 arrives late by 100 generations (Species 1 wins)
+spp1wins = arrival_time(3000, -100, init_pop, 0, type = 0)
+
+# Species 2 arrives late by 100 generations but early by 4 time steps within a generation (\Delta p=4; Species 2 wins)
+spp2wins = arrival_time(3000, -100, init_pop, 4, type = 1)
+
+##### Simulation II. Priority Effects Both within and across Seasons #####
+
+# List of arrival times in generations
+
+early_gens = seq(-100, 20, 5)
+
+# List of relative arrival times (\Delta p) within a generation
+
+deltap_lst = seq(-4, 2, 1)
+
+## Simulation II.i. Numeric Priority Effects ##
+
+# Increase run time to 12000 to get rid of all transients; this takes a while, even with parallelization
+# Run through both lists, collect final populations
+
+num_results = data.frame()
+for (dp in deltap_lst) {
+  endpop = foreach(t = early_gens, .combine = rbind) %dopar% {
+   arrival_time(12000, t, init_pop, dp, type = 0) %>% 
+      tail(1) %>% 
+      mutate(early = t, deltap = dp)
+  }
+  num_results = rbind(endpop, num_results)
+}
+
+# Categorize by outcomes of competition; 
+# consumer is excluded if population of consumers < 1e-2
+# 1, 2, and 3 codes for species 1 wins, 2 wins, and coexistence
+
+num_results %<>% mutate(outcome = if_else(N1 > 1e-2 & N2 > 1e-2, 3, 
+                                          if_else(N1 > 1e-2 & N2 < 1e-2, 1, 2)))
+
+## Simulation II.ii. Trait-mediated Priority Effects ##
+
+tra_results = data.frame()
+for (dp in deltap_lst) {
+  endpop = foreach(t = early_gens, .combine = rbind) %dopar% {
+    arrival_time(12000, t, init_pop, dp, type = 1) %>% 
+      tail(1) %>% 
+      mutate(early = t, deltap = dp)
+  }
+  tra_results = rbind(endpop, tra_results)
+}
+
+# Categorize by outcomes of competition; 
+# consumer is excluded if population of consumers < 1e-2
+# 1, 2, and 3 codes for species 1 wins, 2 wins, and coexistence
+
+tra_results %<>% mutate(outcome = if_else(N1 > 1e-2 & N2 > 1e-2, 3, 
+                                          if_else(N1 > 1e-2 & N2 < 1e-2, 1, 2)))
+
+##### Write Data #####
+
+dta_dir = "Data"
+if (!dir.exists(dta_dir)) {
+  dir.create(dta_dir)
+}
+
+write_csv(spp1wins, "Data/1wins.csv")
+write_csv(spp2wins, "Data/2wins.csv")
+
+write_csv(num_results, "Data/num_results.csv")
+write_csv(tra_results, "Data/tra_results.csv")
+
+##### Figures #####
+
+# Read Data
+
+spp1wins = read_csv("Data/1wins.csv")
+spp2wins = read_csv("Data/2wins.csv")
+
+num_results = read_csv("Data/num_results.csv")
+tra_results = read_csv("Data/tra_results.csv")
+
+## Simulation I. Numeric and Trait-mediated Priority Effects
+
+p_1wins =
+  spp1wins %>% 
+  pivot_longer(cols = 2:3, names_to = "Species", values_to = "Population") %>% 
+  ggplot(aes(x = Time, y = Population, color = Species)) + 
+  geom_line(size = 1.5) + 
+  xlab("Years") +
+  scale_y_continuous(name = "Density", limits = c(0, 1300), breaks = c(500, 1000)) + 
+  # scale_y_log10() + 
+  scale_color_manual(values = color_scheme, labels = c("1", "2")) + 
+  ggtitle("A") + 
+  theme_bw() + 
+  theme(axis.text = element_text(size = 15), axis.title.x = element_text(size = 20), axis.title.y = element_text(size = 20),
+        legend.text = element_text(size = 17), legend.title = element_text(size = 20), 
+        plot.title = element_text(size = 20), panel.grid.minor = element_blank())
+
+p_2wins =
+  spp2wins %>% 
+  pivot_longer(cols = 2:3, names_to = "Species", values_to = "Population") %>% 
+  ggplot(aes(x = Time, y = Population, color = Species)) + 
+  geom_line(size = 1.5) + 
+  xlab("Years") +
+  scale_y_continuous(name = "Density", limits = c(0, 1300), breaks = c(500, 1000)) + 
+  # scale_y_log10() +
+  scale_color_manual(values = color_scheme, labels = c("1", "2")) + 
+  ggtitle("B") + 
+  theme_bw() + 
+  theme(axis.text = element_text(size = 15), axis.title.x = element_text(size = 20), axis.title.y = element_blank(),
+        legend.text = element_text(size = 17), legend.title = element_text(size = 20), 
+        plot.title = element_text(size = 20), panel.grid.minor = element_blank())
+
+# Simulation II: Priority Effects Both within and across Seasons
+
+num_phase = 
+  num_results %>% 
+  ggplot(aes(x = deltap, y = early, fill = as.factor(outcome))) + 
+  geom_tile() + 
+  # geom_rect(color = "black", alpha = 0, 
+  #           aes(xmin = -0.5, xmax = 0.5, ymin = min(early_gens)-2.5, ymax = max(early_gens)+2.5)) + 
+  # geom_rect(color = "black", alpha = 0, linetype = 2, 
+  #           aes(xmin = min(deltap_lst)-0.5, xmax = max(deltap_lst)+0.5, ymin = -2.5, ymax = 2.5)) +
+  scale_fill_manual(values = color_scheme, labels = c("1 wins", "2 wins"), name = "Outcome") + 
+  scale_x_continuous(name = expression(paste("Relative arrival times (", Delta, p, ")")), 
+                     breaks = deltap_lst, label = deltap_lst) +
+  scale_y_continuous(name = "Years early") +
+  ggtitle("C") + 
+  theme_bw() + 
+  theme(axis.text = element_text(size = 15), axis.title.x = element_text(size = 20), axis.title.y = element_text(size = 20),
+        legend.text = element_text(size = 17), legend.title = element_text(size = 20), legend.position = "left",
+        plot.title = element_text(size = 20), panel.grid = element_blank())
+
+tra_phase = 
+  tra_results %>% 
+  ggplot(aes(x = deltap, y = early, fill = as.factor(outcome))) + 
+  geom_tile() + 
+  # geom_rect(color = "black", alpha = 0, 
+  #           aes(xmin = -0.5, xmax = 0.5, ymin = min(early_gens)-2.5, ymax = max(early_gens)+2.5)) + 
+  # geom_rect(color = "black", alpha = 0, linetype = 2, 
+  #           aes(xmin = min(deltap_lst)-0.5, xmax = max(deltap_lst)+0.5, ymin = -2.5, ymax = 2.5)) + 
+  scale_fill_manual(values = color_scheme, labels = c("1 wins", "2 wins", "Coexistence"), name = "Outcome", guide = "none") + 
+  scale_x_continuous(name = expression(paste("Relative arrival times (", Delta, p, ")")), 
+                     breaks = deltap_lst, label = deltap_lst) +
+  scale_y_continuous(name = "Years early") +
+  theme_bw() + 
+  ggtitle("D") +
+  theme(axis.text = element_text(size = 15), axis.title.x = element_text(size = 20), axis.title.y = element_blank(),
+        legend.text = element_text(size = 17), legend.title = element_text(size = 20), legend.position = "left",
+        plot.title = element_text(size = 20), panel.grid = element_blank())
+
+# Create directory
+
+fig_dir = "Figures"
+if (!dir.exists(fig_dir)) {
+  dir.create(fig_dir)
+}
+
+# Composite phase diagram
+
+ggsave("Figures/Fig3I.pdf", 
+       plot = (p_1wins | p_2wins) / (num_phase | tra_phase) + plot_layout(guides = "collect"), 
+       width = 4000, height = 3000, units = "px")
+
+num_phase + tra_phase + tra_deltap0 + num_early0 + tra_early0 + guide_area() + plot_layout(guides = "collect")
+
+##### Extras ##### 
+
+# Won't appear in the final buildup
+
+# Examine final population at \Delta p=0
+
+num_deltap0 = 
+  num_results %>% 
+  filter(deltap == 0) %>% 
+  pivot_longer(cols = 2:3, names_to = "Species", values_to = "Population") %>% 
+  ggplot(aes(x = early, y = Population, color = Species)) +
+  geom_point() + 
+  geom_line() + 
+  scale_color_manual(values = color_scheme, labels = c("1", "2")) + 
+  ylab("log10(Final Population)") +
+  scale_x_continuous(name = "Generations early", breaks = c(0, -40, -80), label = c(0, -40, -80)) +
+  theme_bw() + 
+  # ggtitle("C") + 
+  coord_flip() + 
+  scale_y_log10() +
+  theme(axis.text = element_text(size = 15), axis.title.x = element_text(size = 20), axis.title.y = element_blank(),
+        legend.text = element_text(size = 17), legend.title = element_text(size = 20), 
+        plot.title = element_text(size = 20), panel.grid = element_blank())
+
+# Examine final population when the two species arrive at the same generation
+
+num_early0 = 
+  num_results %>% 
+  filter(early == 0) %>% 
+  pivot_longer(cols = 2:3, names_to = "Species", values_to = "Population") %>% 
+  ggplot(aes(x = deltap, y = Population, color = Species)) +
+  geom_point() + 
+  geom_line() + 
+  scale_y_log10(name = "log10(Final Population)") + 
+  scale_color_manual(values = color_scheme, labels = c("1", "2")) + 
+  scale_x_continuous(name = expression(paste(Delta, p)), breaks = deltap_lst, label = deltap_lst) +
+  theme_bw() + 
+  ggtitle("D") +
+  theme(axis.text = element_text(size = 15), axis.title.x = element_text(size = 20), axis.title.y = element_text(size = 20),
+        legend.text = element_text(size = 17), legend.title = element_text(size = 20), 
+        plot.title = element_text(size = 20), panel.grid = element_blank())
+
+# Examine final population at \Delta p=0
+
+tra_deltap0 = 
+  tra_results %>% 
+  filter(deltap == 0) %>% 
+  pivot_longer(cols = 2:3, names_to = "Species", values_to = "Population") %>% 
+  ggplot(aes(x = early, y = Population, color = Species)) +
+  geom_point() + 
+  geom_line() + 
+  scale_color_manual(values = color_scheme, labels = c("1", "2"), guide = "none") + 
+  scale_y_log10(name = "log10(Final Population)", breaks = c(1e-15, 1e-10, 1e-5, 1)) +
+  scale_x_continuous(name = "Generations early", breaks = c(0, -40, -80), label = c(0, -40, -80)) +
+  theme_bw() + 
+  ggtitle("C") + 
+  coord_flip() + 
+  theme(axis.text = element_text(size = 15), axis.title.x = element_text(size = 20), axis.title.y = element_blank(),
+        legend.text = element_text(size = 17), legend.title = element_text(size = 20), 
+        plot.title = element_text(size = 20), panel.grid = element_blank())
+
+# Examine final population when the two species arrive at the same generation
+
+tra_early0 = 
+  tra_results %>% 
+  filter(early == 0) %>% 
+  pivot_longer(cols = 2:3, names_to = "Species", values_to = "Population") %>% 
+  ggplot(aes(x = deltap, y = Population, color = Species)) +
+  geom_point() + 
+  geom_line() + 
+  scale_y_log10(name = "log10(Final Population)", breaks = c(1e-20, 1e-15, 1e-10, 1e-5, 1)) + 
+  scale_color_manual(values = color_scheme, labels = c("1", "2"), guide = "none") + 
+  scale_x_continuous(name = expression(paste(Delta, p)), breaks = deltap_lst, label = deltap_lst) +
+  theme_bw() + 
+  ggtitle("E") + 
+  theme(axis.text = element_text(size = 15), axis.title.x = element_text(size = 20), axis.title.y = element_blank(),
+        legend.text = element_text(size = 17), legend.title = element_text(size = 20), 
+        plot.title = element_text(size = 20), panel.grid = element_blank())
+
+
+  
---
+++ LitReview.R
@@ -0,0 +1,167 @@
+# Literature Review
+# Hengxing Zou
+
+# packages
+
+library(tidyverse)
+library(magrittr)
+library(readxl)
+library(janitor)
+library(patchwork)
+
+# color scheme from https://jfly.uni-koeln.de/color/
+color_scheme = c("#444444", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#bbbbbb")
+
+##### Read File and Clean Up #####
+
+lit = read_excel("../Review/Categorized.xlsx", sheet = 4)
+
+##### Focal Taxa #####
+
+# Change all input to lower case
+
+lit %<>% mutate(`Focal Taxa` = tolower(`Focal Taxa`), `Secondary Taxa` = tolower(`Secondary Taxa`))
+
+# Parse multiple focal taxa
+
+lit_all_taxa = separate(lit, col = `Focal Taxa`, into = c("Focal Taxa 1", "Focal Taxa 2", "Focal Taxa 3"), sep = "&")
+lit_all_taxa$`Focal Taxa 1` = str_trim(lit_all_taxa$`Focal Taxa 1`)
+lit_all_taxa$`Focal Taxa 2` = str_trim(lit_all_taxa$`Focal Taxa 2`)
+lit_all_taxa$`Focal Taxa 3` = str_trim(lit_all_taxa$`Focal Taxa 3`)
+
+# Tally Taxa
+
+focal_taxa_freq = 
+  lit_all_taxa %>% 
+  pivot_longer(cols = 4:6, names_to = "Type", values_to = "Focal Taxa") %>% 
+  tabyl(`Focal Taxa`, show_na = F) %>% 
+  arrange(n)
+focal_taxa_freq
+
+##### Experimental Design #####
+
+# Temporal treatments
+
+lit %<>% 
+  mutate(`# Manipulated Species Binned` = case_when(`# Manipulated Species` %in% c("1", "2") ~ "1-2", 
+                                                    `# Manipulated Species` %in% as.character(seq(3, 10, 1)) ~ "3-10", 
+                                                    `# Manipulated Species` == ">10" ~ ">10",
+                                                    TRUE ~ "No\ntemporal\ntreatment")) %>% 
+  mutate(`# Manipulated Species Binned` = factor(`# Manipulated Species Binned`, 
+                                                 levels = c("No\ntemporal\ntreatment", "1-2", "3-10", ">10")))
+
+tabyl(lit, `# Manipulated Species Binned`)
+
+temp = 
+  lit %>% 
+  ggplot(aes(x = `# Manipulated Species Binned`)) + 
+  geom_bar(stat = "count", width = 0.6, fill = "#444444") + 
+  geom_text(stat = "count", aes(label = ..count..), hjust = 2, color = "white", size = 6) + 
+  labs(x = "Number of\nmanipulated species", y = "Number of studies") + 
+  # scale_x_discrete(guide = guide_axis(n.dodge = 2)) + 
+  ggtitle("A") + 
+  coord_flip() + 
+  theme_bw() + 
+  theme(axis.text = element_text(size = 15), axis.title.x = element_blank(), axis.title.y = element_text(size = 20),
+        plot.title = element_text(size = 20), panel.grid.minor = element_blank())
+
+# Temporal gradient
+
+tabyl(lit, `Temporal Gradient?`)
+
+##### Durations #####
+
+durations = lit %>% 
+  filter(!(`Duration (Generations)` %in% c("Mixed","Not Applicable"))) %>% 
+  mutate(`Duration (Generations)` = factor(`Duration (Generations)`, 
+                                           levels = c("<=1", "2-5", ">5")))
+
+margin.table(table(durations$`Duration (Generations)`, durations$`Generation Time`), margin = c(1, 2))
+prop.table(table(durations$`Duration (Generations)`, durations$`Generation Time`))
+
+dura = 
+  durations %>% 
+  ggplot(aes(x = `Duration (Generations)`, fill = `Generation Time`)) + 
+  geom_bar() + 
+  geom_text(stat = "count", aes(label = ..count..), hjust = 2.5, color = "white", size = 6) + 
+  # some hard coded values for the extra label
+  geom_text(aes(x = 2, y = 32, label = 4), color = "#E69F00", size = 6) +
+  scale_fill_manual(values = c("#E69F00", "#56B4E9")) + 
+  ylab("Number of studies") + 
+  coord_flip() + 
+  ggtitle("B") + 
+  theme_bw() +
+  theme(axis.text = element_text(size = 15), axis.title.x = element_text(size = 20), axis.title.y = element_text(size = 20),
+        legend.text = element_text(size = 17), legend.title = element_text(size = 20), legend.position = "bottom", 
+        plot.title = element_text(size = 20), panel.grid.minor = element_blank())
+
+##### Taxa and Durations #####
+
+# Including all taxa in the same study
+
+dura_taxa_df = lit_all_taxa %>% 
+  pivot_longer(cols = `Focal Taxa 1`: `Focal Taxa 3`, names_to = "# Taxa", values_to = "Focal Taxa") %>% 
+  drop_na("Focal Taxa") %>% 
+  filter(!(`Duration (Generations)` %in% c("Mixed","Not Applicable"))) %>% 
+  mutate(`Duration (Generations)` = factor(`Duration (Generations)`, 
+                                           levels = c("<=1", "2-5", ">5")))
+  
+# Broader categorization of taxa
+
+dura_taxa_df %<>% 
+  mutate(`Broad Taxa` = case_when(`Focal Taxa` %in% c("fish", "amphibians") ~ "Fish and Amphibians", 
+                                  `Focal Taxa` %in% c("nematodes", "trematodes") ~ "Nematodes and Trematodes", 
+                                  `Focal Taxa` %in% c("insects", "arachnids", "crustacean", "zooplankton", "crustaceans") ~ "Crustaceans", 
+                                  `Focal Taxa` %in% c("terrestrial plants", "aquatic plants", "phytoplankton") ~ "Plants and Algae", 
+                                  `Focal Taxa` %in% c("mycorrhizal/wood-decomposing fungi", "fungi") ~ "Fungi", 
+                                  `Focal Taxa` %in% c("bacteria", "archaea") ~ "Prokaryotes", 
+                                  `Focal Taxa` == "protists" ~ "Protists", 
+                                  `Focal Taxa` == "virus" ~ "Virus", 
+                                  TRUE ~ "Other taxa")) %>% 
+  mutate(`Broad Taxa` = factor(`Broad Taxa`, 
+                               levels = c("Other taxa", "Fish and Amphibians", "Plants and Algae", "Fungi", 
+                                          "Crustaceans", "Protists", "Prokaryotes", "Nematodes and Trematodes", "Virus")))
+
+
+dura_taxa = 
+  dura_taxa_df %>% 
+  ggplot(aes(x = `Duration (Generations)`, fill = `Broad Taxa`)) + 
+  geom_bar(position = "fill") + 
+  scale_fill_manual(values = color_scheme) +
+  scale_y_continuous(name = "Proportion of studies", labels = c("0", "25%", "50%", "75%", "100%")) + 
+  coord_flip() + 
+  ggtitle("C") + 
+  theme_bw() +
+  theme(axis.text = element_text(size = 15), axis.title.x = element_text(size = 20), axis.title.y = element_text(size = 20),
+        legend.text = element_text(size = 15), legend.title = element_text(size = 20), legend.position = "bottom", 
+        plot.title = element_text(size = 20), panel.grid.minor = element_blank())
+
+##### Measurements #####
+
+lit %<>% mutate(`Primary Measurement` = tolower(`Primary Measurement`))
+
+lit_all_meas = separate(lit, col = `Primary Measurement`, into = c("Measurement 1", "Measurement 2", "Measurement 3"), sep = "&")
+lit_all_meas$`Measurement 1` = str_trim(lit_all_meas$`Measurement 1`)
+lit_all_meas$`Measurement 2` = str_trim(lit_all_meas$`Measurement 2`)
+lit_all_meas$`Measurement 3` = str_trim(lit_all_meas$`Measurement 3`)
+
+meas_freq = 
+  lit_all_meas %>% 
+  pivot_longer(cols = 15:17, names_to = "Type", values_to = "Measurements") %>% 
+  tabyl(`Measurements`, show_na = F)
+meas_freq
+
+##### Figures #####
+
+# Create directory
+
+fig_dir = "Figures"
+if (!dir.exists(fig_dir)) {
+  dir.create(fig_dir)
+}
+
+# Result figure
+
+ggsave("Figures/Fig1I.pdf", 
+       plot = temp / dura / dura_taxa, 
+       width = 3000, height = 4000, units = "px")
